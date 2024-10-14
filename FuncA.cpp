1ec2b763d7d76cf9163c39ee712d67d710322eb0
MirosMazurenko <mazurenkomyroslav@gmail.com>
2024-10-14 23:03:52 +0300

    Hotfix

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,14 @@
 #include "FuncA.h"
+#include <cmath>
 
 double FuncA::calculate()
 {
-    return 0;
+    double sum = M_PI / 2;
+    for (int i = 0; i < 3; ++i)
+    {
+        double numerator = factorial(2 * i);
+        double denominator = std::pow(4, i) * std::pow(factorial(i), 2) * (2 * i + 1);
+        sum -= (numerator / denominator) * std::pow(x, 2 * i + 1);
+    }
+    return sum;
 }
